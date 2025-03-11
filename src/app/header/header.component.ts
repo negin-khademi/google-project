1aad354a59e601726b0c01b92ce3be32e2245d04
Negin <negin.khademi@gmail.com>
2025-03-11 15:45:29 +0330

    remove 2 components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
 
-import {LoginComponent} from '../login/login.component';
-import {SignupComponent} from '../signup/signup.component';
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
