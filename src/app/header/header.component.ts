f39d2167cb8d14816647a2eaa5d67432a8422065
Negin <negin.khademi@gmail.com>
2025-03-12 12:04:05 +0330

    styling 2 button in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
