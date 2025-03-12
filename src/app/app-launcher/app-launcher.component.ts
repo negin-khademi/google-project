60c06316ac9ac832b9fd666fd72e1c0e5a53551f
Negin <negin.khademi@gmail.com>
2025-03-12 08:21:53 +0330

    creating app-launcher compenent

---
+++ src/app/app-launcher/app-launcher.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-launcher',
+  imports: [],
+  templateUrl: './app-launcher.component.html',
+  styleUrl: './app-launcher.component.scss'
+})
+export class AppLauncherComponent {
+
+}
