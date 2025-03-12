3a6eceec91603db47c1bae845a22d8e6bb20b150
Negin <negin.khademi@gmail.com>
2025-03-12 13:19:29 +0330

    create google-account component

---
+++ src/app/google-account/google-account.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-google-account',
+  imports: [],
+  templateUrl: './google-account.component.html',
+  styleUrl: './google-account.component.scss'
+})
+export class GoogleAccountComponent {
+
+}
