f38dff1e6d7c3858637a24baee7b414ba76a3055
Negin <negin.khademi@gmail.com>
2025-03-12 13:11:14 +0330

    styling header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { faTh } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [MatButtonModule, MatDividerModule],
+  imports: [MatButtonModule, MatDividerModule, FaIconComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  faTh = faTh;
+}
