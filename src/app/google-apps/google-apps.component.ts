ef82787d6b5d47fe9fe8df466c52ca1311c3c8e2
Negin <negin.khademi@gmail.com>
2025-03-12 13:19:43 +0330

    cerate google-apps component

---
+++ src/app/google-apps/google-apps.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-google-apps',
+  imports: [],
+  templateUrl: './google-apps.component.html',
+  styleUrl: './google-apps.component.scss'
+})
+export class GoogleAppsComponent {
+
+}
