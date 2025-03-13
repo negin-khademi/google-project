48c1fc9aa3f062650b93cc25e8141d012fd12153
Negin <negin.khademi@gmail.com>
2025-03-13 21:09:57 +0330

    app-account component

--- src/app/google-account/google-account.component.ts
+++ src/app/google-account/google-account.component.ts
@@ -1,13 +1,24 @@
+import { CommonModule } from '@angular/common'; // اضافه کردن CommonModule
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; // وارد کردن ماژول FontAwesome
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { ProfileButtonComponent } from '../shared/profile-button/profile-button.component';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'; // وارد کردن آیکن مداد
 
 @Component({
   selector: 'app-google-account',
-  imports: [MatButtonModule, MatMenuModule],
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    ProfileButtonComponent,
+    FontAwesomeModule,
+    CommonModule,
+  ],
   templateUrl: './google-account.component.html',
   styleUrl: './google-account.component.scss',
 })
 export class GoogleAccountComponent {
   profileImage = 'profile.jpg';
+  faPencilAlt = faPencilAlt;
 }
