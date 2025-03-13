5648e58159ddee8bac7bbea9411518e03482af52
Negin <negin.khademi@gmail.com>
2025-03-13 09:20:17 +0330

    use google account components as a menu for showing account information

--- src/app/google-account/google-account.component.ts
+++ src/app/google-account/google-account.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-google-account',
-  imports: [],
+  imports: [MatButtonModule, MatMenuModule],
   templateUrl: './google-account.component.html',
-  styleUrl: './google-account.component.scss'
+  styleUrl: './google-account.component.scss',
 })
 export class GoogleAccountComponent {
-
+  profileImage = 'profile.jpg';
 }
