0fd771ae1b136927a80c8031f654f0e273ce09e8
Negin <negin.khademi@gmail.com>
2025-03-13 21:13:57 +0330

    replace code with tag component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,8 @@
+import { AppLauncherComponent } from '../app-launcher/app-launcher.component';
 import { Component } from '@angular/core';
-import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { GoogleAccountComponent } from '../google-account/google-account.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
-import { faTh } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -11,12 +10,10 @@ import { faTh } from '@fortawesome/free-solid-svg-icons';
     MatButtonModule,
     MatButtonModule,
     MatDividerModule,
-    FaIconComponent,
     GoogleAccountComponent,
+    AppLauncherComponent,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-  faTh = faTh;
-}
+export class HeaderComponent {}
