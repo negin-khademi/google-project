16070e7b48e4318c96571efb99194a8f23e9fa7b
Negin <negin.khademi@gmail.com>
2025-03-13 21:09:25 +0330

    app-launcher component

--- src/app/app-launcher/app-launcher.component.ts
+++ src/app/app-launcher/app-launcher.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatMenuModule } from '@angular/material/menu';
+import { faTh } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-app-launcher',
-  imports: [],
+  imports: [MatMenuModule, FontAwesomeModule, FaIconComponent],
   templateUrl: './app-launcher.component.html',
-  styleUrl: './app-launcher.component.scss'
+  styleUrl: './app-launcher.component.scss',
 })
 export class AppLauncherComponent {
-
+  faTh = faTh;
 }
